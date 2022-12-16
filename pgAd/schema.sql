0b0b2062d88d531fdbafbacfd27d0e030ffbf9f1
feven27 <110137693+feven27@users.noreply.github.com>
2022-12-16 13:33:19 -0500

    climate_change

---
+++ pgAd/schema.sql
@@ -0,0 +1,29 @@
+-- Creating tables for cleanworld_temp
+CREATE TABLE cleanUS_temp (
+     dt date NOT NULL,
+     Avg_temp  varchar,
+     Avg_temp_unc varchar,
+     state varchar NULL,
+     Country varchar NULL,
+     PRIMARY KEY (dt)
+);
+
+
+SELECT * FROM cleanWorld_temp;
+
+-- Creating tables for cleanworld_temp
+CREATE TABLE cleanWorld_temp (
+     dt date NOT NULL,
+	 Land_Avg    varchar NOT NULL,
+     Land_Avg_Uncer  varchar NOT NULL,
+     LandMax_temp    varchar NOT NULL,
+     LandMax_tempUncer varchar NOT NULL,
+     LandMin_temp     varchar NOT NULL,
+     LandMin_tempUncer varchar NOT NULL,
+     LandOceanAvg_temp varchar NOT NULL,
+     LandOceanAvg_tempUncer varchar NOT NULL,
+	FOREIGN KEY (dt) REFERENCES cleanUS_temp (dt),
+	PRIMARY KEY (dt)
+);
+
+SELECT * FROM cleanus_temp;
